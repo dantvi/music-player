14ea2f095aef28917089730c58e31bf22f958507
dantvi <daniel.tving@gmail.com>
2024-03-09 17:50:07 +0100

    calculate & display current time

--- script.js
+++ script.js
@@ -99,16 +99,21 @@ function updateProgressBar(e) {
         progress.style.width = `${progressPercent}%`;
         // Calculate display for duration
         const durationMinutes = Math.floor(duration / 60);
-        console.log('minutes:', durationMinutes);
         let durationSeconds = Math.floor(duration % 60);
         if (durationSeconds < 10) {
             durationSeconds = `0${durationSeconds}`;
         }
-        console.log('seconds:', durationSeconds);
         // Delay switching duration Element to avoid NaN
         if (durationSeconds) {
             durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
         }
+        // Calculate display for current time
+        const currentMinutes = Math.floor(currentTime / 60);
+        let currentSeconds = Math.floor(currentTime % 60);
+        if (currentSeconds < 10) {
+            currentSeconds = `0${currentSeconds}`;
+        }
+        currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
     }
 }
 
